976bc9c4699f01037e19b1f9b7cc948c661b1cf6
a <a>
2024-04-27 14:14:56 +0200

    Initial Commit

---
+++ KW_lab2/KW_lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  KW_lab2App.swift
+//  KW_lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct KW_lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
